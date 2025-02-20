c215b07a3043b4315a311e5718dc1f14b2c7eaf6
LauraNsenga <laura.nsenga@eagles.oc.edu>
2025-02-20 14:54:45 -0600

    GIT IGNORE

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require("./app/routes/user.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT || 3029;
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
